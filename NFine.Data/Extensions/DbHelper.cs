6afd7c2438ee75f70518f79d30b417aa9b638245
lwy <123456>
2018-01-22 14:44:10 +0800

    提交解决方案

---
+++ NFine.Data/Extensions/DbHelper.cs
@@ -0,0 +1,56 @@
+﻿/*******************************************************************************
+ * Copyright © 2016 NFine.Framework 版权所有
+ * Author: NFine
+ * Description: NFine快速开发平台
+ 
+*********************************************************************************/
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+namespace NFine.Data.Extensions
+{
+    public class DbHelper
+    {
+        private static string connstring = ConfigurationManager.ConnectionStrings["NFineDbContext"].ConnectionString;
+
+        /// <summary>
+        /// 要实现数据库备份功能修改此方法
+        /// </summary>
+        /// <param name="cmdText"></param>
+        /// <returns></returns>
+        public static int ExecuteSqlCommand(string cmdText)
+        {
+            using (DbConnection conn = new SqlConnection(connstring))
+            {
+                DbCommand cmd = new SqlCommand();
+                PrepareCommand(cmd, conn, null, CommandType.Text, cmdText, null);
+                return cmd.ExecuteNonQuery();
+            }
+        }
+        //public static int ExecuteSqlCommand(string cmdText)
+        //{
+        //    using (DbConnection conn = new MySqlConnection(connstring))
+        //    {
+        //        DbCommand cmd = new MySqlCommand();
+        //        PrepareCommand(cmd, conn, null, CommandType.Text, cmdText, null);
+        //        return cmd.ExecuteNonQuery();
+        //    }
+        //}
+        private static void PrepareCommand(DbCommand cmd, DbConnection conn, DbTransaction isOpenTrans, CommandType cmdType, string cmdText, DbParameter[] cmdParms)
+        {
+            if (conn.State != ConnectionState.Open)
+                conn.Open();
+            cmd.Connection = conn;
+            cmd.CommandText = cmdText;
+            if (isOpenTrans != null)
+                cmd.Transaction = isOpenTrans;
+            cmd.CommandType = cmdType;
+            if (cmdParms != null)
+            {
+                cmd.Parameters.AddRange(cmdParms);
+            }
+        }
+    }
+}
---
+++ NFine.Repository/SystemManage/ItemsDetailRepository.cs
@@ -0,0 +1,39 @@
+﻿/*******************************************************************************
+ * Copyright © 2016 NFine.Framework 版权所有
+ * Author: NFine
+ * Description: NFine快速开发平台
+ 
+*********************************************************************************/
+using NFine.Data;
+using NFine.Domain.Entity.SystemManage;
+using NFine.Domain.IRepository.SystemManage;
+using NFine.Repository.SystemManage;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace NFine.Repository.SystemManage
+{
+    public class ItemsDetailRepository : RepositoryBase<ItemsDetailEntity>, IItemsDetailRepository
+    {
+        public List<ItemsDetailEntity> GetItemList(string enCode, string itemCode)
+        {
+            StringBuilder strSql = new StringBuilder();
+            strSql.Append(@"SELECT  d.*");
+            strSql.Append(@"FROM Sys_ItemsDetail d");
+            strSql.Append(@" INNER JOIN Sys_Items i ON i.F_Id = d.F_ItemId");
+            strSql.Append(@" WHERE 1 = 1");
+            strSql.Append(@" AND i.F_EnCode = '" + enCode + "'");
+            strSql.Append(@" AND d.F_EnabledMark = 1");
+            if (itemCode != "")
+                strSql.Append(@" AND d.F_ItemCode ='" + itemCode + "'");
+            strSql.Append(@" ORDER BY d.F_SortCode ASC");
+            //DbParameter[] parameter = 
+            //{
+            //     new SqlParameter("@enCode",enCode)
+            //};
+            return this.FindList(strSql.ToString());
+        }
+    }
+}
